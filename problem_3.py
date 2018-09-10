57d62f9e26b1151bb1091d9c201ff789a4d8efba
Grae Drake <graemdrake@gmail.com>
2018-09-09 23:56:47 -0700

    Solve Problem 3

---
+++ problem_3.py
@@ -0,0 +1,30 @@
+"""Solve Project Euler Problem 3.
+
+Problem statement: The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143?
+
+https://projecteuler.net/problem=3
+"""
+
+import argparse
+from datetime import datetime
+from tools import prime_factors
+
+
+def solution(limit: int) -> int:
+    """Factor into primes and return the largest"""
+    return prime_factors(limit)[-1]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("number", help="number, default to 13195",
+                    default=13195, type=int, nargs='?')
+    args = parser.parse_args()
+    start = datetime.now()
+    answer = solution(args.number)
+    end = datetime.now()
+    print("The answer is {} for input {}.".format(answer, args.number))
+    print("Execution time was {}.".format(end - start))
+    
