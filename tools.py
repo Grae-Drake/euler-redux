f3248aa326078aca4302922c77f3a664db2ac791
Grae Drake <graemdrake@gmail.com>
2018-09-09 23:56:47 -0700

    Solve Problem 3

--- tools.py
+++ tools.py
@@ -17,3 +17,22 @@ def arithmetic_series_sum(first_term: int, last_term: int, n: int) -> int:
     """
 
     return int(n * (first_term + last_term) / 2)
+
+
+def prime_factors(n: int):
+    """Return a sorted list of unique prime factors of n."""
+
+    candidate_primes = prime_sieve(int(n ** 0.5))
+    return [prime for prime in candidate_primes if n % prime == 0]
+
+
+def prime_sieve(limit: int):
+    # """Generate primes below limit with the Sieve of Eratosthenes."""
+    primes = [True] * limit
+    i = 2
+    while(i < limit):
+        if primes[i]:
+            for j in range(2 * i, limit, i):
+                primes[j] = False
+        i += 1
+    return [i for i, val in list(enumerate(primes))[2:] if val]
